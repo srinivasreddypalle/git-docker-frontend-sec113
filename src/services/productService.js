93ff7649161fc36cbb5c4f2caf8cabaaeafce57d
srinivasreddypalle <2300032622@kluniversity.in>
2025-09-24 16:46:35 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-13-233-104-19.ap-south-1.compute.amazonaws.com:8086/api/products";  // Backend API URL
+const API_BASE_URL = "http://ec2-13-204-86-150.ap-south-1.compute.amazonaws.com:8086/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
