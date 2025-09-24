204cdc55db95a4b7b4cdd70c3239aafc08c3ba0a
srinivasreddypalle <2300032622@kluniversity.in>
2025-09-24 18:29:31 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-13-204-86-150.ap-south-1.compute.amazonaws.com:8086/api/cart";
+const API_URL = "http://ec2-65-2-140-225.ap-south-1.compute.amazonaws.com:8086/api/cart";
 
-const API_BASE = "http://ec2-13-204-86-150.ap-south-1.compute.amazonaws.com:8086/api/cart";
+const API_BASE = "http://ec2-65-2-140-225.ap-south-1.compute.amazonaws.com:8086/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${API_BASE}/add`, {
